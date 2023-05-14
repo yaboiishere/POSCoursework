90d28d13f4775c33dc683e7f21c1835e7ca5a5b9
Mihail Dobrev <mixaildobrev@gmail.com>
2023-05-14 21:47:18 +0300

    add demo

--- main.c
+++ main.c
@@ -5,40 +5,43 @@
 sem_t *sem;
 void f1() {
   while (1) {
+    sem_wait(sem);
     printf("-");
+    sleep(1);
+    sem_post(sem);
   }
 }
 void f2() {
   while (1) {
     printf("+");
+    sleep(1);
   }
 }
 
 void f3() {
-  sem_wait(sem);
-  sleep(20);
-  sem_post(sem);
   while (1) {
+    sem_wait(sem);
     printf("*");
+    sleep(1);
+    sem_post(sem);
   }
 }
 
 void f4() {
   while (1) {
     printf("/");
+    sleep(1);
   }
 }
 
 int main() {
   init_library();
   sem = sem_init(1);
-  create_task(f4);
-  create_task(f3);
-  sem_wait(sem);
-  sleep(5);
-  sem_post(sem);
   create_task(f2);
+  create_task(f4);
+
   create_task(f1);
+  create_task(f3);
 
   while (1)
     ;
